b3e6d65a35d24781733c203b0cf5b8edb09b1e52
sergeigrishchenko <s.i.grishchenko@gmail.com>
2021-10-10 23:54:01 +0300

    Fix nullability for null keyword

--- src/converter/plugins/convertOptionalUnionType.ts
+++ src/converter/plugins/convertOptionalUnionType.ts
@@ -1,23 +1,24 @@
-import ts, {SyntaxKind} from "typescript";
+import ts, {Node, SyntaxKind} from "typescript";
 import {ConverterPlugin} from "../plugin";
 
+const isNull = (type: Node) => ts.isLiteralTypeNode(type) && type.literal.kind === SyntaxKind.NullKeyword
+const isUndefined = (type: Node) => type.kind === SyntaxKind.UndefinedKeyword
+
+const isNullable = (type: Node) => isNull(type) || isUndefined(type)
+
 export const convertOptionalUnionType: ConverterPlugin = (node, context, render) => {
-    if (
-        ts.isUnionTypeNode(node) &&
+    if (ts.isUnionTypeNode(node) &&
         node.types.length === 2 &&
-        node.types.some(type => (
-            type.kind === SyntaxKind.NullKeyword ||
-            type.kind === SyntaxKind.UndefinedKeyword
-        ))
+        node.types.some(type => isNullable(type))
     ) {
         context.cover(node)
         context.cover(node.types[0])
         context.cover(node.types[1])
 
-        const nonNullableType = node.types.find((type => (
-            type.kind !== SyntaxKind.NullKeyword &&
-            type.kind !== SyntaxKind.UndefinedKeyword
-        )))
+        const nonNullableType = node.types.find(type => !isNullable(type))
+        const nullableType = node.types.find(type => isNullable(type))
+
+        nullableType && context.deepCover(nullableType)
 
         return (nonNullableType && `${render(nonNullableType)}?`) ?? ""
     }
