ba43315890acb1346214151863f63321565c39f7
sergeigrishchenko <s.i.grishchenko@gmail.com>
2022-12-16 19:01:59 +0200

    Perform generation before KotlinJsCompile tasks

--- gradle-plugin/karakum-gradle-plugin/src/main/kotlin/org/jetbrains/karakum/gradle/plugin/KarakumPlugin.kt
+++ gradle-plugin/karakum-gradle-plugin/src/main/kotlin/org/jetbrains/karakum/gradle/plugin/KarakumPlugin.kt
@@ -5,9 +5,11 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.registering
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.karakum.gradle.plugin.tasks.KarakumConfig
 import org.jetbrains.karakum.gradle.plugin.tasks.KarakumGenerate
 import org.jetbrains.karakum.gradle.plugin.tasks.KarakumPluginsCopy
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 
 class KarakumPlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = with(project) {
@@ -25,6 +27,10 @@ class KarakumPlugin : Plugin<Project> {
             group = KARAKUM_GRADLE_PLUGIN_GROUP
 
             dependsOn(configureKarakum)
+        }
+
+        tasks.withType<KotlinJsCompile> {
+            dependsOn(generateKarakumExternals)
         }
     }
 }
