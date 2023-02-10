8515916d3749a29d9f3b40e7fffa56bc83fe42e9
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-02-10 17:55:29 +0200

    Gradle plugin is migrated to Kotlin multiplatform extension

--- gradle-plugin/karakum-gradle-plugin/src/main/kotlin/io/github/sgrishchenko/karakum/gradle/plugin/tasks/Tasks.kt
+++ gradle-plugin/karakum-gradle-plugin/src/main/kotlin/io/github/sgrishchenko/karakum/gradle/plugin/tasks/Tasks.kt
@@ -2,7 +2,8 @@ package io.github.sgrishchenko.karakum.gradle.plugin.tasks
 
 import io.github.sgrishchenko.karakum.gradle.plugin.KARAKUM_CONFIG_FILE
 import org.gradle.api.Task
-import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJsCompilation
 import org.jetbrains.kotlin.gradle.targets.js.npm.npmProject
@@ -10,7 +11,7 @@ import java.io.File
 
 val Task.kotlinJsCompilation: KotlinJsCompilation
     get() {
-        val extension = project.extensions.getByType(KotlinJsProjectExtension::class.java)
+        val extension = project.extensions.getByType<KotlinMultiplatformExtension>()
         val target = extension.js()
 
         return target.compilations.getByName(KotlinCompilation.MAIN_COMPILATION_NAME)
