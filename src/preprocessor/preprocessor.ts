2d15a13bce04d509b69ecaef39d76dd626589f58
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-04-08 22:46:52 +0300

    Preprocessor prototype is removed

--- src/preprocessor/preprocessor.ts
+++
@@ -1,8 +0,0 @@
-import ts, {SourceFile, TransformerFactory} from "typescript";
-
-export function preprocess(sourceFile: SourceFile, transformers: TransformerFactory<SourceFile>[]): SourceFile {
-    const result = ts.transform(sourceFile, transformers);
-    const [output] = result.transformed
-
-    return output
-}
