e8e5be1f0fcffdaba97a40f9720e9d9acbed7601
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-07-13 16:26:03 +0300

    Required options are added to CLI

--- src/cli.ts
+++ src/cli.ts
@@ -3,15 +3,24 @@
 import yargs from 'yargs'
 import {hideBin} from 'yargs/helpers'
 import {process as mainProcess} from "./process.js";
-import {PartialConfiguration} from "./configuration/configuration.js";
 
 const argv = yargs(hideBin(process.argv))
+    .option("input", {
+        type: "string",
+        array: true,
+        demandOption: true,
+        description: "Input glob patterns",
+    })
+    .option("output", {
+        type: "string",
+        demandOption: true,
+        description: "Output directory or file"
+    })
     .config("config")
     .parserConfiguration({
         "dot-notation": false
     })
     .parseSync()
 
-// TODO: Specify all config params explicitly
-mainProcess(argv as unknown as PartialConfiguration)
+mainProcess(argv)
     .catch(error => console.error(error))
