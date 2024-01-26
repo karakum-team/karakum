479b61c5449271d185ed05e616b89e20775213d7
sergeigrishchenko <s.i.grishchenko@gmail.com>
2024-01-26 12:53:13 +0200

    Handle assertions in type predicates

--- src/converter/plugins/convertTypePredicate.ts
+++ src/converter/plugins/convertTypePredicate.ts
@@ -1,6 +1,7 @@
 import ts from "typescript";
 import {createSimplePlugin} from "../plugin.js";
 import {CheckCoverageService, checkCoverageServiceKey} from "./CheckCoveragePlugin.js";
+import {TypeScriptService, typeScriptServiceKey} from "./TypeScriptPlugin.js";
 
 export const convertTypePredicate = createSimplePlugin((node, context, render) => {
     if (!ts.isTypePredicateNode(node)) return null
@@ -8,7 +9,13 @@ export const convertTypePredicate = createSimplePlugin((node, context, render) =
     const checkCoverageService = context.lookupService<CheckCoverageService>(checkCoverageServiceKey)
     checkCoverageService?.deepCover(node)
 
+    const typeScriptService = context.lookupService<TypeScriptService>(typeScriptServiceKey)
+
     // TODO: support contracts
 
-    return "Boolean"
+    const type = Boolean(node.assertsModifier)
+        ? "Unit"
+        : "Boolean"
+
+    return `${type} /* ${typeScriptService?.printNode(node)} */`
 })
