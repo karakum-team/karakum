c785aade6acec20f05fa0aa7426d53481d536121
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-01-20 02:22:19 +0200

    Convert few plugins

---
+++ gradle-plugin/karakum/src/jsMain/kotlin/io/github/sgrishchenko/karakum/extension/plugins/InheritanceModifierPlugin.kt
@@ -0,0 +1,90 @@
+package io.github.sgrishchenko.karakum.extension.plugins
+
+import io.github.sgrishchenko.karakum.extension.*
+import js.array.ReadonlyArray
+import js.symbol.Symbol
+import typescript.Node
+
+val inheritanceModifierServiceKey = Symbol()
+
+class InheritanceModifierService(private val inheritanceModifiers: ReadonlyArray<InheritanceModifier<Node>>) {
+    fun resolveSignatureInheritanceModifier(
+        node: Node,
+        signature: Signature,
+        context: ConverterContext,
+    ): String? {
+        val inheritanceModifierContext = object : InheritanceModifierContext, ConverterContext by context {
+            override val signature = signature
+            override val getter = false
+            override val setter = false
+        }
+
+        return internalResolveInheritanceModifier(node, inheritanceModifierContext)
+    }
+
+    fun resolveGetterInheritanceModifier(
+        node: Node,
+        context: ConverterContext,
+    ): String? {
+        val inheritanceModifierContext = object : InheritanceModifierContext, ConverterContext by context {
+            override val signature = null
+            override val getter = true
+            override val setter = false
+        }
+
+        return internalResolveInheritanceModifier(node, inheritanceModifierContext)
+    }
+
+    fun resolveSetterInheritanceModifier(
+        node: Node,
+        context: ConverterContext,
+    ): String? {
+        val inheritanceModifierContext = object : InheritanceModifierContext, ConverterContext by context {
+            override val signature = null
+            override val getter = false
+            override val setter = true
+        }
+
+        return internalResolveInheritanceModifier(node, inheritanceModifierContext)
+    }
+
+    fun resolveInheritanceModifier(
+        node: Node,
+        context: ConverterContext,
+    ): String? {
+        val inheritanceModifierContext = object : InheritanceModifierContext, ConverterContext by context {
+            override val signature = null
+            override val getter = false
+            override val setter = false
+        }
+
+        return internalResolveInheritanceModifier(node, inheritanceModifierContext)
+    }
+
+    private fun internalResolveInheritanceModifier(
+        node: Node,
+        context: InheritanceModifierContext
+    ): String? {
+        for (inheritanceModifier in inheritanceModifiers) {
+            val result = inheritanceModifier(node, context)
+
+            if (result != null) return result
+        }
+
+        return null
+    }
+}
+
+class InheritanceModifierPlugin(inheritanceModifiers: ReadonlyArray<InheritanceModifier<Node>>) : ConverterPlugin<Node> {
+    private val inheritanceModifierService = InheritanceModifierService(inheritanceModifiers)
+
+    override fun setup(context: ConverterContext) {
+        context.registerService(inheritanceModifierServiceKey, this.inheritanceModifierService)
+    }
+
+    override fun traverse(node: Node, context: ConverterContext) = Unit
+
+    override fun render(node: Node, context: ConverterContext, next: Render<Node>) = null
+
+    override fun generate(context: ConverterContext, render: Render<Node>) = emptyArray<GeneratedFile>()
+}
