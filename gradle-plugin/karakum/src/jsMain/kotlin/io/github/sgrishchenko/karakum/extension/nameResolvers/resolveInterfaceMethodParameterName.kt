4d91948f6ea0ad95f562676be6aa3e55adcd3fc5
porotkin <preload737773@gmail.com>
2025-01-22 22:05:42 +0200

    Convert `resolveInterfaceMethodParameterName`

---
+++ gradle-plugin/karakum/src/jsMain/kotlin/io/github/sgrishchenko/karakum/extension/nameResolvers/resolveInterfaceMethodParameterName.kt
@@ -0,0 +1,41 @@
+package io.github.sgrishchenko.karakum.extension.nameResolvers
+
+import io.github.sgrishchenko.karakum.extension.NameResolver
+import io.github.sgrishchenko.karakum.extension.plugins.TypeScriptService
+import io.github.sgrishchenko.karakum.extension.plugins.typeScriptServiceKey
+import io.github.sgrishchenko.karakum.util.capitalize
+import io.github.sgrishchenko.karakum.util.getParentOrNull
+import typescript.*
+
+val resolveInterfaceMethodParameterName = NameResolver<Node> { node, context ->
+    val typeScriptService = context.lookupService<TypeScriptService>(typeScriptServiceKey)
+    val getParent = { it: Node ->
+        typeScriptService?.getParent(it) ?: it.getParentOrNull()
+    }
+
+    val parameter = getParent(node)
+        ?: return@NameResolver null
+    if (!isParameter(parameter)) return@NameResolver null
+
+    val parameterNameNode = parameter.name
+    if (!isIdentifier(parameterNameNode)) return@NameResolver null
+
+    val parameterName = parameterNameNode.text
+
+    val method = getParent(parameter)
+        ?: return@NameResolver null
+    if (!isMethodSignature(method)) return@NameResolver null
+
+    val methodNameNode = method.name
+    if (!isIdentifier(methodNameNode)) return@NameResolver null
+
+    val methodName = methodNameNode.text
+
+    val interfaceNode = getParent(method)
+        ?: return@NameResolver null
+    if (!isInterfaceDeclaration(interfaceNode)) return@NameResolver null
+
+    val parentName = interfaceNode.name.text
+
+    "${capitalize(parentName)}${capitalize(methodName)}${capitalize(parameterName)}"
+}
