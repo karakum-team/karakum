5b3792c952ca34ed1842eae857045fec9ce1128b
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-02-04 22:10:41 +0200

    Add tests for extension

--- gradle-plugin/karakum/src/jsTest/functional/io/github/sgrishchenko/karakum/extension/ExtensionTest.kt
+++ gradle-plugin/karakum/src/jsTest/functional/io/github/sgrishchenko/karakum/extension/ExtensionTest.kt
@@ -1,11 +1,22 @@
 package io.github.sgrishchenko.karakum.extension
 
+import io.github.sgrishchenko.karakum.configuration.PartialConfiguration
+import io.github.sgrishchenko.karakum.generateTests
+import js.objects.recordOf
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ExtensionTest {
     @Test
-    fun test() {
-        assertEquals(expected = true, actual = true)
+    fun test() = runTest {
+        generateTests("extension") { output ->
+            recordOf<String, Any?>().apply {
+                this["input"] = "**/*.d.ts"
+                this["output"] = output
+                this["libraryName"] = "extension"
+                this["verbose"] = true
+                this["extensions"] = "./extensions.js"
+            }.unsafeCast<PartialConfiguration>()
+        }
     }
 }
