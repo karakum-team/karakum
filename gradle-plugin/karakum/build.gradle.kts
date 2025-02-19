8fc79830001edc86e84f4d896c82d1e7404ba134
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-02-19 23:41:42 +0200

    Prepare karakum subproject to npm publishing

--- gradle-plugin/karakum/build.gradle.kts
+++ gradle-plugin/karakum/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec
+
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.js-plain-objects")
@@ -25,11 +27,12 @@ kotlin {
         compilations.named("main") {
             packageJson {
                 customField("exports", "./kotlin/karakum.mjs")
-                customField("bin", "./kotlin/karakum-bin.mjs")
+                customField("bin", mapOf("karakum" to "kotlin/karakum-bin.mjs"))
+                customField("scripts", mapOf("publish" to "npm publish --dry-run"))
             }
         }
     }
-    
+
     sourceSets {
         jsMain.dependencies {
             implementation(libs.coroutines.core)
@@ -56,10 +59,26 @@ kotlin {
     }
 }
 
-tasks.named<ProcessResources>("jsTestProcessResources") {
-    expand(
-        "functionalTestLib" to layout.projectDirectory.dir("src/jsTest/resources").asFile.path,
-        "functionalTestGenerated" to layout.projectDirectory.dir("src/jsTest/generated").asFile.path,
-        "functionalTestOutput" to layout.buildDirectory.dir("karakum/output").get().asFile.path,
+tasks.named<ProcessResources>("jsProcessResources") {
+    from(
+        rootProject.layout.projectDirectory.file("LICENSE"),
+        rootProject.layout.projectDirectory.file("README.md"),
     )
+}
+
+tasks.named<ProcessResources>("jsTestProcessResources") {
+    filesMatching("test.config.json") {
+        expand(
+            "functionalTestLib" to layout.projectDirectory.dir("src/jsTest/resources").asFile.path,
+            "functionalTestGenerated" to layout.projectDirectory.dir("src/jsTest/generated").asFile.path,
+            "functionalTestOutput" to layout.buildDirectory.dir("karakum/output").get().asFile.path,
+        )
+    }
+}
+
+NodeJsExec.create(
+    compilation = kotlin.js().compilations.getByName("main"),
+    name = "npmPublish",
+) {
+    args("--run", "publish")
 }
