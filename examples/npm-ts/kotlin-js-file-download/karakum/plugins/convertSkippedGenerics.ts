8c52f42da794ebd262121733ada26d75c111e20f
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-04-28 22:53:25 +0300

    Update npm-based examples

--- examples/npm-ts/kotlin-js-file-download/karakum/plugins/convertSkippedGenerics.ts
+++ examples/npm-ts/kotlin-js-file-download/karakum/plugins/convertSkippedGenerics.ts
@@ -1,8 +1,6 @@
 import ts, {type Node} from "typescript"
 import type {Context} from "karakum"
 
-console.log("NODE_OPTIONS", process.env.NODE_OPTIONS)
-
 export default function (node: Node, context: Context, render: (node: Node) => string) {
     if (
         ts.isTypeReferenceNode(node)
