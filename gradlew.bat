6a2232368517145191c774ff554a29cb0ed3bedb
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-04-29 19:44:05 +0300

    Check wrapper-validation

--- gradlew.bat
+++ gradlew.bat
@@ -24,7 +24,7 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRNAME=%~dp0
+set DIRN AME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
 @rem This is normally unused
 set APP_BASE_NAME=%~n0
