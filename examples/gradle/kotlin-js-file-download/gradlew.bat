aa53f88f739aafc226754cb721f18c231892e8cb
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-04-29 19:35:13 +0300

    Check wrapper-validation

--- examples/gradle/kotlin-js-file-download/gradlew.bat
+++ examples/gradle/kotlin-js-file-download/gradlew.bat
@@ -1,7 +1,7 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
-@rem Licensed un der the Apache License, Version 2.0 (the "License");
+@rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
 @rem
