88c2ae39698178052176eede04bcd18ff0b66626
sergeigrishchenko <s.i.grishchenko@gmail.com>
2026-02-22 23:22:51 +0200

    Provide Matcher DSL

---
+++ karakum/src/jsMain/kotlin/io/github/sgrishchenko/karakum/extension/Matcher.kt
@@ -0,0 +1,95 @@
+package io.github.sgrishchenko.karakum.extension
+
+import io.github.sgrishchenko.karakum.util.getParentOrNull
+import typescript.NamedDeclaration
+import typescript.Node
+import typescript.isIdentifier
+
+interface Matcher {
+    val predicate: (Node) -> Boolean
+    val children: List<Matcher>
+}
+
+private class MatcherImpl(
+    override val predicate: (Node) -> Boolean,
+    override val children: List<Matcher>,
+) : Matcher
+
+private class MatcherDelegateImpl(
+    override val predicate: (Node) -> Boolean,
+    val childrenProvider: () -> List<Matcher>,
+) : Matcher {
+    override val children: List<Matcher>
+        get() = childrenProvider()
+}
+
+interface MatcherScope {
+    val children: MutableList<Matcher>
+}
+
+private class MatcherScopeImpl(
+    override val children: MutableList<Matcher> = mutableListOf(),
+) : MatcherScope
+
+fun MatcherScope.match(
+    predicate: (Node) -> Boolean,
+    block: MatcherScope.() -> Unit = { },
+) {
+    val scope = MatcherScopeImpl().also(block)
+    val matcher = MatcherImpl(predicate, scope.children)
+    children.add(matcher)
+}
+
+fun MatcherScope.match(predicate: (Node) -> Boolean): MatcherScope {
+    val scope = MatcherScopeImpl()
+    val matcher = MatcherDelegateImpl(predicate, scope::children)
+    children.add(matcher)
+    return scope
+}
+
+private fun ((Node) -> Boolean).withName(name: String): (Node) -> Boolean {
+    return { node ->
+        @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
+        this(node) && (node as NamedDeclaration).name.let { it != null && isIdentifier(it) && it.text == name }
+    }
+}
+
+fun MatcherScope.match(
+    predicate: (Node) -> Boolean,
+    name: String,
+    block: MatcherScope.() -> Unit,
+) {
+    match(predicate.withName(name), block)
+}
+
+fun MatcherScope.match(
+    predicate: (Node) -> Boolean,
+    name: String
+): MatcherScope {
+    return match(predicate.withName(name))
+}
+
+fun match(
+    block: MatcherScope.() -> Unit,
+): List<Matcher> {
+    val scope = MatcherScopeImpl().also(block)
+    return scope.children
+}
+
+private fun Matcher.toPredicateChain(): List<(Node) -> Boolean> {
+    if (children.isEmpty()) return listOf(predicate)
+    return children.flatMap { it.toPredicateChain() + predicate }
+}
+
+fun Matcher.matches(node: Node): Boolean {
+    val predicates = toPredicateChain()
+
+    var current = node
+
+    for (predicate in predicates) {
+        if (!predicate(node)) return false
+        current = current.getParentOrNull() ?: return false
+    }
+
+    return true
+}
