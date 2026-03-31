57c32eef0c51c94d9b683b1016cb7658c98194fd
sergeigrishchenko <s.i.grishchenko@gmail.com>
2026-03-31 21:24:33 +0300

    Update npm examples

--- examples/npm-js/kotlin-js-file-download/generated/fileDownload.kt
+++ examples/npm-js/kotlin-js-file-download/generated/fileDownload.kt
@@ -6,8 +6,8 @@ package js.file.download
 
 external fun fileDownload(data: String, filename: String, mime: String = definedExternally, bom: String = definedExternally): Unit
 
-external fun fileDownload(data: ArrayBuffer, filename: String, mime: String = definedExternally, bom: String = definedExternally): Unit
+external fun fileDownload(data: js.buffer.ArrayBuffer, filename: String, mime: String = definedExternally, bom: String = definedExternally): Unit
 
-external fun fileDownload(data: ArrayBufferView, filename: String, mime: String = definedExternally, bom: String = definedExternally): Unit
+external fun fileDownload(data: js.buffer.ArrayBufferView, filename: String, mime: String = definedExternally, bom: String = definedExternally): Unit
 
-external fun fileDownload(data: Blob, filename: String, mime: String = definedExternally, bom: String = definedExternally): Unit
+external fun fileDownload(data: web.blob.Blob, filename: String, mime: String = definedExternally, bom: String = definedExternally): Unit
