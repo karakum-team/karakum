8fcf6bb5fb3ae565c5537cc628dfe46c808a7ddf
sergeigrishchenko <s.i.grishchenko@gmail.com>
2026-04-04 22:14:25 +0300

    Update package lock

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,10 @@ plugins {
 
 plugins.withType<NodeJsPlugin> {
     val typescriptVersion: String by project
+    val mochaVersion: String by project
 
-    the<NodeJsRootExtension>().versions.typescript.version = typescriptVersion
+    the<NodeJsRootExtension>().versions.apply {
+        typescript.version = typescriptVersion
+        mocha.version = mochaVersion
+    }
 }
