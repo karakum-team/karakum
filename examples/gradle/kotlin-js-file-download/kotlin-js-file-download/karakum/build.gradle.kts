7ef19ab9b5f28bcfcb1b98dde7766cc350ecbc8e
sergeigrishchenko <s.i.grishchenko@gmail.com>
2026-04-04 22:50:13 +0300

    Update package lock in examples

--- examples/gradle/kotlin-js-file-download/kotlin-js-file-download/karakum/build.gradle.kts
+++ examples/gradle/kotlin-js-file-download/kotlin-js-file-download/karakum/build.gradle.kts
@@ -1,3 +1,8 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import kotlin.apply
+
 plugins {
     id("io.github.sgrishchenko.karakum") version "1.0.0-alpha.104"
 }
@@ -10,5 +15,11 @@ karakum {
     library {
         name = "js-file-download"
         version = "0.4.12"
+    }
+}
+
+plugins.withType<NodeJsPlugin> {
+    the<NodeJsRootExtension>().versions.apply {
+        mocha.version = "12.0.0-beta-10"
     }
 }
