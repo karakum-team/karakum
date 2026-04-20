7ac4bb828de0b9f660b7795db330d707e28d7fff
sergeigrishchenko <s.i.grishchenko@gmail.com>
2026-04-20 22:41:23 +0300

    Provide second example for NPM+TS setup

---
+++ examples/npm-ts/packages/kotlin-tanstack-history/generated/CreateMemoryHistoryOpts.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package tanstack.history
+
+external interface CreateMemoryHistoryOpts {
+var initialEntries: js.array.ReadonlyArray<String>
+var initialIndex: Double?
+}
